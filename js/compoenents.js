31824d01a333d5c0e390453f2b3dca4650e76c39
NelsonDJCR <dev@nelsondjcr.com>
2024-04-10 13:39:55 -0500

    add compoennets.js file

---
+++ js/compoenents.js
@@ -0,0 +1,32 @@
+const SERVICES = [
+    {
+        TITLE: 'branding',
+        ICON: 'branding',
+        DESCRIPTION: 'Brindamos soluciones de creación y manejo de marca, generando una identidad visual que exprese los valores de tu marca '
+    },
+    {
+        TITLE: 'social media',
+        ICON: 'social-media',
+        DESCRIPTION: 'Conectamos con tu audiencia en las redes sociales de esta forma atraemos y captamos tu publico objetivo, manejando de forma coherente tu marca'
+    },
+    {
+        TITLE: 'Audio-visual',
+        ICON: 'Audio-visual',
+        DESCRIPTION: 'generamos contenido multimedia de alta calidad, alineando estándares y objetivos para garantizar una comunicación visual efectiva'
+    },
+    {
+        TITLE: 'merchandise',
+        ICON: 'merchandise',
+        DESCRIPTION: 'Creamos productos personalizados para tu marca, con el fin de que puedas ofrecer a tus clientes una experiencia única y personalizada'
+    },
+    {
+        TITLE: 'diseño ux ui',
+        ICON: 'diseño-ux-ui',
+        DESCRIPTION: 'Diseñamos interfaces digitales centradas en el usuario, con el fin de que la experiencia de navegación sea intuitiva y agradable'
+    },
+    {   
+        TITLE: 'edición',
+        ICON: 'edición',
+        DESCRIPTION: 'Realizamos retoques fotográficos profesionales, para que tus imágenes tengan la calidad que tu marca necesita'
+    },
+]
